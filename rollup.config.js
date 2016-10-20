4cfe3d9bf756f058eaf4c6c8f95d04852941fd5a
Micah Stubbs <micah.stubbs@gmail.com>
2016-10-19 19:25:10 -0700

    add version of d3-voronoi with voronoi.find

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import json from 'rollup-plugin-json';
 import babel from 'rollup-plugin-babel';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
   entry: 'index.js',
@@ -9,6 +10,10 @@ export default {
     lodash: '_'
   },
   moduleName: 'd3VoronoiScatterplot',
-  plugins: [ json(), babel() ],
-  dest: 'build/bundle.js'
+  plugins: [nodeResolve({ jsnext: true, main: true }), json(), babel()],
+  external: ['lodash'],
+  dest: 'build/bundle.js',
+  acorn: {
+    allowReserved: true
+  }
 };
